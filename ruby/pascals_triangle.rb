b104457bcd624427d516e38681afb281ab666d18
phoenix-xiao <xiaobin.phoenix@gmail.com>
2020-11-30 22:57:43 +0800

    commit LC#118 by ruby

--- ruby/pascals_triangle.rb
+++ ruby/pascals_triangle.rb
@@ -1,7 +1,7 @@
 =begin
  * LC#118 Pascal's Triangle 杨辉三角
  * Link: https://leetcode-cn.com/problems/pascals-triangle/solution/yang-hui-san-jiao-by-leetcode/
- * 思路：根据 num_rows 构建数组层数，杨辉三角可以归类为动态规划，核心思路是基于上一层 prevRow 保存的数据，来构建当前层 row 数组
+ * 思路：根据 num_rows 构建数组层数，杨辉三角可以归类为动态规划，核心思路是基于上一层 prev_row 保存的数据，来构建当前层 row 数组
  * 执行效率：O(n2)
 =end
 
@@ -11,11 +11,11 @@ def pascals_triangle (num_rows)
   triangle.first.push(1)
   row_num = 1
   while row_num < num_rows
-    prevRow = triangle[row_num - 1]
+    prev_row = triangle[row_num - 1]
     row = [].push(1)
     j = 1
     while j < row_num
-      row.push(prevRow[j-1] + prevRow[j])
+      row.push(prev_row[j-1] + prev_row[j])
       j+=1
     end
     row.push(1)
