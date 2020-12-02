5b2905068335167b5a8655f43c2f4be4d992918a
phoenix-xiao <xiaobin.phoenix@gmail.com>
2020-12-02 23:46:15 +0800

    commit LC#172

--- ruby/factorial_trailing_zeroes.rb
+++ ruby/factorial_trailing_zeroes.rb
@@ -1,22 +1,24 @@
 =begin
  LC#172 Factorial Trailing Zeroes 计算阶乘后的零
  Link：https://leetcode-cn.com/problems/factorial-trailing-zeroes/
- 思路：先计算阶乘，然后通过求模%运算计算零的数量（达不到官方要求的 O(log n) 时间复杂度）
+ 思路1 O（n） 解法：计算阶乘：先计算循环计算阶乘，然后通过求模%运算计算零的数量（达不到官方要求的 O(log n) 时间复杂度），提交 CASE 会超出时间限制
+ 思路2 O(log n) 解法，计算因子: 思路1的解法太慢，分析题目会发现阶乘尾部的零是由计算因子5来决定，所以只要计算n里面5的数量，就可以做到作者要求的对数级别 O(logN) 的运行效率
+ 总结：首先想到的是 思路1，思路2 是看了官方解题才有的思路，看评论区貌似还有一种稳定的对数级别的高效计算因子的解法，看看有没有小伙伴去研究一下，总体感觉这道题确实有点难。。花了差不多 2小时
 =end
 
 def trailing_zeroes (n)
-    n_factorial = 1
-    i = 2
-    for i in 2..n
-        n_factorial *= i
-    end
-    zero_count = 0
-    while n_factorial % 10 == 0
-        zero_count += 1
-        n_factorial /= 10
-    end
-    zero_count
+   zero_count = 0
+   i = 5
+   while i <= n
+        current_factor = i
+        while current_factor % 5 == 0
+            zero_count += 1
+            current_factor /= 5
+        end
+        i += 5
+   end
+   zero_count
 end
 
-r = trailing_zeroes(13)
+r = trailing_zeroes(30)
 p "r > ", r
--- src/main/java/algorithms/java/easy/FactorialTrailingZeroes.java
+++ src/main/java/algorithms/java/easy/FactorialTrailingZeroes.java
@@ -1,35 +1,28 @@
 package algorithms.java.easy;
 
-import java.math.BigInteger;
-
 /**
  * LC#172 Factorial Trailing Zeroes 计算阶乘后的零
  * Link：https://leetcode-cn.com/problems/factorial-trailing-zeroes/
- * 思路：先计算阶乘，然后通过求模%运算计算零的数量（达不到官方要求的 O(log n) 时间复杂度）
- *
+ * 思路1 O（n） 解法：计算阶乘：先计算循环计算阶乘，然后通过求模%运算计算零的数量（达不到官方要求的 O(log n) 时间复杂度），提交 CASE 会超出时间限制
+ * 思路2 O(log n) 解法，计算因子: 思路1的解法太慢，分析题目会发现阶乘尾部的零是由计算因子5来决定，所以只要计算n里面5的数量，就可以做到作者要求的对数级别 O(logN) 的运行效率
+ * 总结：首先想到的是 思路1，思路2 是看了官方解题才有的思路，看评论区貌似还有一种稳定的对数级别的高效计算因子的解法，看看有没有小伙伴去研究一下，总体感觉这道题确实有点难。。花了差不多 2小时
  */
 public class FactorialTrailingZeroes {
 
     public static int trailingZeroes(int n) {
-        // 这里使用 BigInteger 计算阶乘，防止阶乘导致 Integer 溢出
-        // Calculate n!
-        BigInteger nFactorial = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            nFactorial = nFactorial.multiply(BigInteger.valueOf(i));
-        }
-
-        // Count how many 0's are on the end.
         int zeroCount = 0;
-        BigInteger mod = nFactorial.mod(BigInteger.TEN);
-        while (mod.equals(BigInteger.ZERO)) {
-            nFactorial = nFactorial.divide(BigInteger.TEN);
-            zeroCount++;
+        for (int i = 5; i <= n; i+=5) {
+            int currentFactor = i;
+            while (currentFactor % 5 == 0) {
+                zeroCount++;
+                currentFactor /= 5;
+            }
         }
         return zeroCount;
     }
 
     public static void main(String[] args) {
-        int r = FactorialTrailingZeroes.trailingZeroes(13);
+        int r = FactorialTrailingZeroes.trailingZeroes(10);
         System.out.println(r);
     }
 }
