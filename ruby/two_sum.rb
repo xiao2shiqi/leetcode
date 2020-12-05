e29e48d4f907ad3696c1c5a005c283298584626d
phoenix.xiao <phoenix.xiao@rccchina.com>
2020-12-05 16:27:20 +0800

    commit two sum by ruby runtime beats !!!

--- ruby/two_sum.rb
+++ ruby/two_sum.rb
@@ -2,7 +2,7 @@
  * LC#1 Two Sum 两数之和
  * https://leetcode-cn.com/problems/two-sum/
  * 思路1：2次哈希求解：将数组放入散列表，遍历散列表，通过target-key得出匹配数组，并且判断非自身元素，返回数组，时间复杂度 O（n）
- * 思路2[runtime beats（最优解）]：1次哈希求解：在遍历 num 数组，如果哈希表不包含 target - num[i] ，则将 num[i], i 放入哈希表，如果包含则直接返回 [i, hash[val]] 下标
+ * 思路2[runtime beats（最优解）]：1次哈希求解：在遍历 num 数组，如果哈希表不包含 target - num[i] ，则将 num[i], i 放入哈希表，如果包含则直接返回 [hash[num[i], i]] 下标
 =end
 def two_sum (nums, target)
 		hash = {}
