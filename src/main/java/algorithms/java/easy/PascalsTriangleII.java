4d8e4f9131a6682f9560c4dd93eacc126e5e33df
phoenix-xiao <xiaobin.phoenix@gmail.com>
2020-12-09 23:21:24 +0800

    commit LC#119  Pascal Triangle II 杨辉三角 II Runtime beats

--- src/main/java/algorithms/java/easy/PascalsTriangleII.java
+++ src/main/java/algorithms/java/easy/PascalsTriangleII.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * LC#119 Pascal's Triangle II 杨辉三角 II
  * Link：https://leetcode-cn.com/problems/pascals-triangle-ii/
- * 思路1：直接套用 LC#117 解法，返回 row 指定层的数组即可，缺点：代码多并且复杂，空间复杂度差，甚至达不到作者要求 O(k) 的要求
+ * 思路1：直接套用 LC#118 解法，返回 row 指定层的数组即可，缺点：代码多并且复杂，空间复杂度差，甚至达不到作者要求 O(k) 的要求
  * 思路2（runtime beats）：直接套用组合公式 c = c*(n-m)/(m+1)，直接计算当前层，符合空间复杂度 O(k) 的要求，比解法1代码量少了很多，性能也更好
  */
 public class PascalsTriangleII {
