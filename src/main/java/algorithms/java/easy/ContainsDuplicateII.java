d84467175b97a026a7d59fff80ab66bead75f563
phoenix-xiao <xiaobin.phoenix@gmail.com>
2020-12-11 23:45:14 +0800

    commit LC#283 Move Zeroes 移动零

---
+++ src/main/java/algorithms/java/easy/ContainsDuplicateII.java
@@ -0,0 +1,14 @@
+package algorithms.java.easy;
+
+/**
+ * LC#219：重复元素 II Contains Duplicate II
+ * Link：https://leetcode-cn.com/problems/contains-duplicate-ii/
+ * 思路1：
+ */
+public class ContainsDuplicateII {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        return false;
+    }
+}
