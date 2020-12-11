e0a5af55488d25b355f5261211ff1c091d427bb2
phoenix-xiao <xiaobin.phoenix@gmail.com>
2020-12-11 23:51:50 +0800

    commit LC#283 Move Zeroes 移动零

--- src/main/java/algorithms/java/easy/MoveZeroes.java
+++ src/main/java/algorithms/java/easy/MoveZeroes.java
@@ -3,7 +3,7 @@ package algorithms.java.easy;
 /**
  * LC#283 Move Zeroes 移动零
  * Link：https://leetcode-cn.com/problems/move-zeroes/
- * 思路1：思路很简单通过指针i，将所有非0元素向前移动，最后将 指针i...数组.length 区间全部填充零，线性时间复杂度，不使用额外的空间，Java beats 100%
+ * 思路1：思路很简单通过指针i，将所有非0元素向前移动，最后将 指针i...数组.length 区间全部填充零，线性时间复杂度，不使用额外的空间，直接干到了 java beats 100%
  */
 public class MoveZeroes {
 
