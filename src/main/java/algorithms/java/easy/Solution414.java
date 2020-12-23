6c507f722091ab294a5553e2f7026dc4a1258bbb
phoenix.xiao <phoenix.xiao@rccchina.com>
2020-12-23 12:52:51 +0800

    commit LC#414 by Java

--- src/main/java/algorithms/java/easy/Solution414.java
+++ src/main/java/algorithms/java/easy/Solution414.java
@@ -2,63 +2,48 @@ package algorithms.java.easy;
 
 import com.sun.source.tree.Tree;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * LC#414 第三大的数 Third Maximum Number
  * Link：https://leetcode-cn.com/problems/third-maximum-number/
- * 思路1：使用有序哈希 TreeSet， 然后取出倒数第三的元素，如果没有取出 last （最大）元素
+ * 思路1（非最优）：使用 sort 和 hash 将数组排序 + 去重，然后根据结果集决定取倒数第三大的数字，还是最大的数字
  * @author Phoenix on 2020/12/23 8:40 上午
  */
 
 public class Solution414 {
 
-
     public int thirdMax(int[] nums) {
-        TreeSet<Integer> treeSet = new TreeSet<>();
+        // 使用 sort + List 实现排序 + 去重
+//        Arrays.sort(nums);
+//        List<Integer> list = new ArrayList<>();
+//        for (int num : nums) {
+//            if (!list.contains(num)) list.add(num);
+//        }
+//        return list.size() >= 3 ? list.get(list.size() - 3) : list.get(list.size() - 1);
+
+        // 使用 TreeSet 实现排序 + 去重
+        Set<Integer> set = new TreeSet<>(Comparator.reverseOrder());
         for (int num : nums) {
-            treeSet.add(num);
+            set.add(num);
         }
-        if (treeSet.size() >= 3) {
-            // 存在第三大的元素
-            int i = 0, targetValue = treeSet.size() - 3;
-            for (Integer integer : treeSet) {
-                if (i == targetValue) return integer;
-                i += 1;
-            }
+        Iterator<Integer> iterator = set.iterator();
+        if (set.size() < 3) {
+            return iterator.next();
         }
-        // 直接返回最大元素
-        return treeSet.last();
+        iterator.next();
+        iterator.next();
+        return iterator.next();
     }
 
     public static void main(String[] args) {
 
 //        int [] nums = {3, 2, 1};
-        int [] nums = {1, 2, -2147483648};
+        int [] nums = {1, 2};
 
         Solution414 solution414 = new Solution414();
         System.out.println("max number :" + solution414.thirdMax(nums));
 
-//        TreeSet<Integer> treeSet = new TreeSet();
-//        treeSet.add(1);
-//        treeSet.add(2);
-//        treeSet.add(3);
-//        treeSet.add(4);
-//        treeSet.add(5);
-//        treeSet.add(6);
-//
-//        int i = 0;
-//        int targetValue = treeSet.size() - 3;
-//        for (Integer integer : treeSet) {
-//            if (i == targetValue) {
-//
-//                System.out.println("target: " + integer);
-//            }
-//            i += 1;
-//        }
 
     }
 }
