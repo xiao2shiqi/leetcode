7f1b28f8a4acd336b973724d3bc8d23b6110c4da
phoenix-xiao <xiaobin.phoenix@gmail.com>
2021-03-28 23:09:42 +0800

    solution 509 by  go

---
+++ go/509-solution.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	n := 4
+	res := fib(n)
+	fmt.Println("res>", res)
+}
