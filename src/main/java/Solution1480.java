f748e003127dcc5b72a0bb13f326321caac93723
phoenix <xiaobin.phoenix@gmail.com>
2021-05-29 17:09:01 +0800

    solution 1480 by java

---
+++ src/main/java/Solution1480.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/**
+ * LC#1480:一维数组动态和
+ * Link: https://leetcode-cn.com/problems/running-sum-of-1d-array/
+ * 思路：动态规划 DP 普通题
+ * @author Phoenix on 2021/5/29.
+ */
+public class Solution1480 {
+
+    public static int[] runningSum(int[] nums) {
+        int lastValue = 0;
+        for (int i = 0; i < nums.length; i++) {
+            lastValue += nums[i];
+            nums[i] = lastValue;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+
+}
