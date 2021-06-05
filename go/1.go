067f1989309963ae0abd4e1eb19962b7e487a5fb
phoenix <xiaobin.phoenix@gmail.com>
2021-06-05 21:41:35 +0800

    rename java

--- go/1.go
+++ go/1.go
@@ -8,20 +8,20 @@ import "fmt"
  * 思路1：2次哈希求解：将数组放入散列表，遍历散列表，通过target-key得出匹配数组，并且判断非自身元素，返回数组，时间复杂度 O（n）
  * 思路2[runtime beats（最优解）]：1次哈希求解：在遍历 num 数组，如果哈希表不包含 target - num[i] ，则将 num[i], i 放入哈希表，如果包含则直接返回 [hash[num[i], i]] 下标
  */
- func twoSum(nums []int, target int) []int {
+func twoSum(nums []int, target int) []int {
 	hashMap := map[int]int{}
 	for i := 0; i < len(nums); i++ {
-		p, ok := hashMap[target - nums[i]]
+		p, ok := hashMap[target-nums[i]]
 		if ok {
 			return []int{p, i}
 		}
 		hashMap[nums[i]] = i
 	}
 	return nil
- }
+}
 
 func main() {
-	// 声明 slice 
+	// 声明 slice
 	nums := []int{2, 7, 11, 15}
 	target := 9
 	p := twoSum(nums, target)
